838928db2528c922c4ced7b5a748f3d9252c27eb
zensquiddy <zensquiddy@gmail.com>
2024-09-25 13:50:17 -0700

    First push

---
+++ PythonWebApp.py
@@ -0,0 +1,10 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__) 
+
+@app.route("/")
+def render_main():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
