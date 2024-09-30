2826c9d41e720f27992841868a36300314d51fd1
zensquiddy <zensquiddy@gmail.com>
2024-09-30 14:19:47 -0700

    getting better

--- PythonWebApp.py
+++ PythonWebApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) 
 
@@ -6,5 +6,31 @@ app = Flask(__name__)
 def render_main():
     return render_template('index.html')
 
+
+
+@app.route("/response")
+def render_response():
+    Name = request.args['Name']
+    Age = request.args['Age']
+    Gender = request.args['Gender']
+    Education = request.args['Education']
+    Homeschooled = request.args['Homeschooled']
+    HomeschoolEducation = request.args['HomeschoolEducation']
+    Major = request.args['Major']
+    Establishment = request.args['Establishment']
+    Preferance = request.args['Preferance']
+    
+    
+    
+    reply1 = "My Name is " + Name + ", I am " + Age + " years old. " + "I am a " + Gender + " person." + " My Highest education is " + Education + "." + " I was " + Homeschooled + " Homeschooled."
+    if HomeschoolEducation != "":
+        reply1 = reply1 + " The Highest education I recieved while homeschooled was " + HomeschoolEducation + "."
+    if Major != "":
+        reply1 = reply1 + "I Majored in " + Major + "." 
+    
+    reply1 = reply1 + " I recived my highest education at " + Establishment + ". My prefered job is a " + Preferance + "."
+        
+    return render_template('response.html', response = reply1)
+    
 if __name__=="__main__":
     app.run(debug=True)
